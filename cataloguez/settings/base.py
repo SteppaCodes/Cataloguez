9ac4c04bc0c0299aa5071125483ab8494dbd1585
SteppaCodes <steppacodes@gmail.com>
2024-03-04 21:50:00 +0100

    chore: project set up

--- cataloguez/settings/base.py
+++ cataloguez/settings/base.py
@@ -16,6 +16,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 DJANGO_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
