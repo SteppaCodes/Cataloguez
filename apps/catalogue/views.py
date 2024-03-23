e21c9793d112bf60a22bd58e68bb1681bce0f702
Big Steppa <steppacodes@gmail.com>
2024-03-23 09:49:04 +0100

    Add: Tag page result fitering

--- apps/catalogue/views.py
+++ apps/catalogue/views.py
@@ -20,16 +20,14 @@ import cloudinary
 class PhotoListView(ListView):
     model = Photo
     paginate_by = 10
-    template_name = 'catalogue/photos.html'
-    context_object_name = 'photos'
+    template_name = "catalogue/photos.html"
+    context_object_name = "photos"
 
     def get_queryset(self):
         qs = super().get_queryset()
-        query = self.request.GET.get('query')
+        query = self.request.GET.get("query")
         if query:
-            qs = qs.filter(
-                Q(title__icontains=query)
-            )
+            qs = qs.filter(Q(title__icontains=query))
         return qs
 
 
@@ -41,35 +39,32 @@ class PhotoDetalView(View):
         tags = photo.tags.all()
 
         # Create a list of the id of all the tags used in the photo
-        photo_tag_ids = tags.values_list('id', flat=True)
+        photo_tag_ids = tags.values_list("id", flat=True)
         # Get all photos that have the tags in the photo_tag_ids list excluding the photo itself
-        related_photos = Photo.objects.filter(tags__in=photo_tag_ids).exclude(id=id).distinct()
+        related_photos = (
+            Photo.objects.filter(tags__in=photo_tag_ids).exclude(id=id).distinct()
+        )
         # Count the number of tags that are the same in similar photos and return them from highest to lowest
-        related_photos = related_photos.annotate(same_tags=Count('tags')).order_by('-same_tags')
+        related_photos = related_photos.annotate(same_tags=Count("tags")).order_by(
+            "-same_tags"
+        )
 
-
-        context = {
-            'photo':photo,
-            "tags":tags,
-            "related_photos": related_photos
-        }
+        context = {"photo": photo, "tags": tags, "related_photos": related_photos}
         return render(request, "catalogue/photo-detail.html", context)
 
 
 class VideosListView(ListView):
     model = Video
     paginate_by = 10
-    template_name = 'catalogue/videos.html'
-    context_object_name = 'videos'
+    template_name = "catalogue/videos.html"
+    context_object_name = "videos"
 
     def get_queryset(self):
         qs = super().get_queryset()
-        query = self.request.GET.get('query')
+        query = self.request.GET.get("query")
 
         if query:
-            qs = qs.filter(
-                Q(title__icontains=query)
-            )
+            qs = qs.filter(Q(title__icontains=query))
         return qs
 
 
@@ -80,53 +75,49 @@ class VideoDetailView(View):
         video.save()
         tags = video.tags.all()
 
-        video_tag_ids = tags.values_list('id', flat=True)
-        related_videos = Video.objects.filter(tags__in=video_tag_ids).exclude(id=id).distinct()
-        related_videos = related_videos.annotate(same_tags=Count('tags')).order_by('-same_tags')
+        video_tag_ids = tags.values_list("id", flat=True)
+        related_videos = (
+            Video.objects.filter(tags__in=video_tag_ids).exclude(id=id).distinct()
+        )
+        related_videos = related_videos.annotate(same_tags=Count("tags")).order_by(
+            "-same_tags"
+        )
 
-        context = {
-            'video':video,
-            "tags":tags,
-            "related_videos": related_videos
-        }
-        return render(request, 'catalogue/video-detail.html', context)
+        context = {"video": video, "tags": tags, "related_videos": related_videos}
+        return render(request, "catalogue/video-detail.html", context)
 
 
 class TagDetailView(View):
     def get(self, request, id):
+        query = request.GET.get("query")
+        if not query:
+            query = ""
+
         tag = get_object_or_404(Tag, id=id)
-        #fix this
-        photos = Photo.objects.filter(tags=tag)
-        videos = Video.objects.filter(tags=tag)
+        photos = Photo.objects.filter(Q(tags=tag) & Q(title__icontains=query))
+        videos = Video.objects.filter(Q(tags=tag) & Q(title__icontains=query))
         media_count = photos.count() + videos.count()
 
-        context = {
-            "tag":tag,
-            "photos":photos,
-            "videos":videos,
-            "count":media_count
-        }
-        return render(request, 'catalogue/tag-medias.html', context)
+        context = {"tag": tag, "photos": photos, "videos": videos, "count": media_count}
+        return render(request, "catalogue/tag-medias.html", context)
 
 
 class UploadMediaRequestView(View):
-   def get(self, request):
-        return render(request, 'catalogue/upload-type.html')
+    def get(self, request):
+        return render(request, "catalogue/upload-type.html")
 
 
-class UploadMediaView(View):    
+class UploadMediaView(View):
     def get(self, request, type):
-        
+
         if type == "photo":
             form = PhotoForm()
         else:
             form = VideoForm()
 
-        context = {
-            "form":form
-        }
-        return render(request, 'catalogue/upload-media.html', context)
-    
+        context = {"form": form}
+        return render(request, "catalogue/upload-media.html", context)
+
     def post(self, request, type):
         if type == "photo":
             form = PhotoForm(request.POST, request.FILES)
@@ -138,42 +129,37 @@ class UploadMediaView(View):
         if form.is_valid():
             media = form.save(commit=False)
             media.user = request.user
-            media.format=format
+            media.format = format
             media.save()
-            sweetify.success(request, 
-                             title="sent",
-                             text="Your Message was sent successfully",
-                             timer=300000
+            sweetify.success(
+                request,
+                title="sent",
+                text="Your Message was sent successfully",
+                timer=300000,
             )
             # create the manytomany relationship for each tag selected
-            media.tags.set(form.cleaned_data['tags'])
-            return redirect('photos')
+            media.tags.set(form.cleaned_data["tags"])
+            return redirect("photos")
 
-        context = {
-            "form":form
-        }
-        return render(request, 'catalogue/upload-media.html', context)
+        context = {"form": form}
+        return render(request, "catalogue/upload-media.html", context)
 
 
 class DownloadMediaView(View, LoginRequiredMixin):
     def get(self, request, id):
         file, media = _get_media_file_(id=id)
 
-        
-        
         if file:
             file_url = file.image_url if media == "img" else file.video_url
-            
+
             # Fetch the file from Cloudinary
             response = cloudinary.api.download(file_url)
-            
+
             # Set the appropriate content disposition for downloading
             response["Content-Disposition"] = f'attachment; filename="{file.file.name}"'
-            
+
             # Return the file as a response
             return response
-            
+
         # Handle the case where file is not found
-        raise FileNotFoundError("File not found")    
-
-
+        raise FileNotFoundError("File not found")
